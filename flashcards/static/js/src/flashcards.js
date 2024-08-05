956ea7bcbdfeef02118ae33c8edd01a7c6b78320
Farhaan Bukhsh <farhaan@opencraft.com>
2024-08-05 16:28:56 +0530

    feat: Add the ability to use the data from XBlock
    Reduces relying on the HTML for data and queries from the XBlock. This
    way we can do the manipulation easily and instead of split view, we have
    unified view of the card.
    
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- flashcards/static/js/src/flashcards.js
+++ flashcards/static/js/src/flashcards.js
@@ -1,36 +1,25 @@
 /* Javascript for FlashcardsXBlock. */
-function FlashcardsXBlock(runtime, element, init_args) {
-  /* Show the answer of the question by cliking on the question */
-  $('.fc-question').click(function () {
-    $('.fc-answer').css('visibility', 'visible');
-  });
-
+function FlashcardsXBlock(runtime, element, params) {
   /* Initializing the variables for displaying the current question
      and total questions.
   */
-  var current_number = parseInt($('.current-fc').text()) + 1;
-  var total_number = parseInt($('.fc-total').text());
+  var currentNumber = -1;
+  var totalNumber = params.flashcards.length;
 
-  /* Hide all elements for the first iteration */
-  $('.flashcards_block li').hide();
+  /* Show the answer of the question by cliking on the question */
+  $('#fc-question').click(function () {
+    $('#fc-question').html(`${params.flashcards[currentNumber]["back"]}`)
+  });
+
   $('.next-btn').click(function () {
-
-    /* Hide the answer again */
-    $('.fc-answer').css('visibility', 'hidden');
-
-    /* Removing the previous question. Have trouble figuring out how to
-       make it work without removing the previous question.
-       TO-DO: Figure this out!
-    */
-    $('.flashcards_block > ul > li:visible:first').remove();
-    $('.flashcards_block > ul > li:hidden:first').show();
-    $('.next-btn').html('NEXT');
-    $('.current-fc').html(current_number++);
-
+    currentNumber++;
+    $('#fc-question').html(`${params.flashcards[currentNumber]["front"]}`);
+    $('#current-fc').html(`${currentNumber + 1}`);
+    $('.next-btn').html('Nextt');
     /* If the student reaches the end say FINISHED and disable going further */
-    if (current_number == total_number + 1) {
-      $('.btn').html('You did it!');
-      $('.btn').addClass('finished-btn').removeClass('next-btn').off('click');
+    if (currentNumber == totalNumber - 1) {
+      $('.next-btn').html('You did it!');
+      $('.next-btn').addClass('finished-btn').removeClass('next-btn').off('click');
     }
   });
 }

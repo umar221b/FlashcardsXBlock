308487f90965cc6924444fb020769bbf320b9a4c
Farhaan Bukhsh <farhaan@opencraft.com>
2024-08-05 10:00:38 +0530

    fix: Fixes the formating issue with JS and Python files
    Signed-off-by: Farhaan Bukhsh <farhaan@opencraft.com>

--- flashcards/static/js/src/flashcards.js
+++ flashcards/static/js/src/flashcards.js
@@ -1,37 +1,38 @@
 /* Javascript for FlashcardsXBlock. */
-function FlashcardsXBlock(runtime, element) {
-    /* Show the answer of the question by cliking on the question */
-    $('.fc-question').click(function() {
-        $('.fc-answer').css('visibility', 'visible');
-    });
+function FlashcardsXBlock(runtime, element, init_args) {
+  /* Show the answer of the question by cliking on the question */
+  $('.fc-question').click(function () {
+    $('.fc-answer').css('visibility', 'visible');
+  });
 
-    /* Initializing the variables for displaying the current question
-       and total questions.
+  /* Initializing the variables for displaying the current question
+     and total questions.
+  */
+  var current_number = parseInt($('.current-fc').text()) + 1;
+  var total_number = parseInt($('.fc-total').text());
+  debugger;
+
+  /* Hide all elements for the first iteration */
+  $('.flashcards_block li').hide();
+  $('.next-btn').click(function () {
+
+    /* Hide the answer again */
+    $('.fc-answer').css('visibility', 'hidden');
+
+    /* Removing the previous question. Have trouble figuring out how to
+       make it work without removing the previous question.
+       TO-DO: Figure this out!
     */
-    var current_number = parseInt($('.current-fc').text())+1;
-    var total_number = parseInt($('.fc-total').text());
+    $('.flashcards_block > ul > li:visible:first').remove();
+    $('.flashcards_block > ul > li:hidden:first').show();
+    $('.next-btn').html('NEXT');
+    $('.current-fc').html(current_number++);
 
-    /* Hide all elements for the first iteration */
-    $('.flashcards_block li').hide();
-    $('.next-btn').click(function() {
-
-        /* Hide the answer again */
-        $('.fc-answer').css('visibility', 'hidden');
-
-        /* Removing the previous question. Have trouble figuring out how to
-           make it work without removing the previous question.
-           TO-DO: Figure this out!
-        */
-        $('.flashcards_block > ul > li:visible:first').remove();
-        $('.flashcards_block > ul > li:hidden:first').show();
-        $('.next-btn').html('NEXT');
-        $('.current-fc').html(current_number++);
-
-        /* If the student reaches the end say FINISHED and disable going further */
-        if (current_number == total_number+1) {
-            $('.btn').html('You did it!');
-            $('.btn').addClass('finished-btn').removeClass('next-btn').off('click');
-        }
-    });
+    /* If the student reaches the end say FINISHED and disable going further */
+    if (current_number == total_number + 1) {
+      $('.btn').html('You did it!');
+      $('.btn').addClass('finished-btn').removeClass('next-btn').off('click');
+    }
+  });
 }
 
--- flashcards/static/js/src/flashcards_edit.js
+++ flashcards/static/js/src/flashcards_edit.js
@@ -1,32 +1,32 @@
-function FlashcardsEditXBlock(runtime, element) {
-    $(element).find('.save-button').bind('click', function() {
-        var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+function FlashcardsEditXBlock(runtime, element, init_args) {
+  $(element).find('.save-button').bind('click', function () {
+    var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
 
 
-        /* Every flashcard (fc-item) has two input fields, "front" and "back" */
-        var flashcard_list = {};
-        var items = document.getElementsByClassName('fc-item');
+    /* Every flashcard (fc-item) has two input fields, "front" and "back" */
+    var flashcard_list = {};
+    var items = document.getElementsByClassName('fc-item');
 
-        for (var i = 0; i < items.length; i++) {
-        /* Read every flashcard and save input values to a dictionary object */
-            var inputs = items[i].getElementsByTagName('input');
-            flashcard_list[inputs[0].value] = inputs[1].value;
-        }
+    for (var i = 0; i < items.length; i++) {
+      /* Read every flashcard and save input values to a dictionary object */
+      var inputs = items[i].getElementsByTagName('input');
+      flashcard_list[inputs[0].value] = inputs[1].value;
+    }
 
-        var data = {
-            title: $(element).find('input[name=title]').val(),
-            flashcards: flashcard_list
-        };
+    var data = {
+      title: $(element).find('input[name=title]').val(),
+      flashcards: flashcard_list
+    };
 
-        var test = JSON.stringify(data);
+    var test = JSON.stringify(data);
 
-        runtime.notify('save', {state: 'start'});
-        $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
-            runtime.notify('save', {state:'end'});
-        });
+    runtime.notify('save', { state: 'start' });
+    $.post(handlerUrl, JSON.stringify(data)).done(function (response) {
+      runtime.notify('save', { state: 'end' });
     });
+  });
 
-    $(element).find('.cancel-button').bind('click', function() {
-        runtime.notify('cancel', {});
-    });
+  $(element).find('.cancel-button').bind('click', function () {
+    runtime.notify('cancel', {});
+  });
 }

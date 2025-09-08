b7eb6b15ce6f9b2234cf5ba968fc53690409effc
Umar Alkafaween <30433769+umar221b@users.noreply.github.com>
2025-09-08 09:36:05 +0300

    Fix display on button

--- flashcards/static/js/src/flashcards.js
+++ flashcards/static/js/src/flashcards.js
@@ -15,7 +15,6 @@ function FlashcardsXBlock(runtime, element, params) {
   function next() {
     if (currentNumber == -1) {
       $('.prev-btn').show();
-      $('.next-btn').html('Next');
     }
     currentNumber++;
     $('.fc-card').removeClass('is-flipped');
@@ -23,6 +22,7 @@ function FlashcardsXBlock(runtime, element, params) {
     $('#fc-question').html(`${params.flashcards[currentNumber]["front"]}`);
     $('#fc-answer').html(`${params.flashcards[currentNumber]["back"]}`)
     $('#current-fc').html(`${currentNumber + 1}`);
+    $('.next-btn').html('Next');
     if (currentNumber > 0)
       $('.prev-btn').removeAttr('disabled');
     /* If the student reaches the end say FINISHED and disable going further */
